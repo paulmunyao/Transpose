9e74eda1c7712fd4419d825014be453480a391f4
paulmunyao <paulmunyao094@gmail.com>
2023-04-05 18:08:55 +0300

    feat(serializers.py-&-views.py): add feature to reset and change password
    a user can change and reset their password
    
    BREAKING CHANGE:

--- backend/serializers.py
+++ backend/serializers.py
@@ -2,18 +2,31 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 # user serializer
+
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id','username', 'email')
+        fields = ('id', 'username', 'email')
 
 # Register serializer
+
+
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id','username','email','password')
+        fields = ('id', 'username', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username'] ,validated_data['email'], validated_data['password'])  
-        return user 
+        user = User.objects.create_user(
+            # trunk-ignore(ruff/E501)
+            validated_data['username'], validated_data['email'], validated_data['password'])
+        user.save()
+        return user
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    model = User
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
