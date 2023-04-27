e8de44d102b46fe0b8f721863d3e97cf2128afbf
paulmunyao <paulmunyao094@gmail.com>
2023-04-27 12:33:30 +0300

    fix(models.py): rename User classes
    add more fields in the user class
    
    BREAKING CHANGE:

--- backend/models.py
+++ backend/models.py
@@ -1,28 +1,17 @@
 from django.db import models
 from django.utils import timezone
-from django.db import transaction
-
-from django.contrib.auth.models import (
-    AbstractBaseUser, PermissionsMixin, BaseUserManager, Group, Permission)
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('input email')
-        try:
-            with transaction.atomic():
-                user = self.model(email=email, **extra_fields)
-                user.set_password(password)
-                user.save(using=self._db)
-                return user
-        except:
-            raise
-
-    def created_user(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
-        return self. create_user(email, password, **extra_fields)
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
@@ -31,21 +20,14 @@ class UserManager(BaseUserManager):
         return self.create_superuser(email, password, **extra_fields)
 
 
-class CustomUser(AbstractBaseUser, PermissionsMixin):
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(max_length=50, unique=True)
     first_name = models.CharField(max_length=50, blank=True)
     last_name = models.CharField(max_length=50, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
-    groups = models.ManyToManyField(
-        Group, verbose_name='groups', blank=True,
-        related_name='customuser_set'  # set a different related name for groups
-    )
-    user_permissions = models.ManyToManyField(
-        Permission, verbose_name='user permissions', blank=True,
-        related_name='customuser_set'  # set a different related name for user permissions
-    )
 
     objects = UserManager()
 
@@ -53,5 +35,5 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def save(self, *args, **kwargs):
-        super(CustomUser, self).save(*args, **kwargs)
+        super(User, self).save(*args, **kwargs)
         return self
--- backend/serializers.py
+++ backend/serializers.py
@@ -1,17 +1,18 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from .models import User
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username','email', 'first_name', 'last_name', 'password')
+        fields = ('id', 'username', 'email',
+                  'first_name', 'last_name', 'password')
         extra_kwargs = {'password': {'write_only': True}, }
 
-        def create(self, validated_data):
-            user = User.objects.create_user(validated_data['username'],
-                                            email=validated_data['email'],
-                                            password=validated_data['password'])
-            return user
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['username'],
+                                        email=validated_data['email'],
+                                        password=validated_data['password'])
+        return user
 
 # User Serializer
 
