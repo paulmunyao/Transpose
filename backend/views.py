3f28406002ac10be77d1857da0fbe5e85904e95b
paulmunyao <paulmunyao094@gmail.com>
2023-05-24 15:57:52 +0300

    fix(models.py): add table to user
    BREAKING CHANGE:

--- backend/views.py
+++ backend/views.py
@@ -42,10 +42,10 @@ class RegisterUser(generics.GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        # token, created = Token.objects.get_or_create(user=user)
+        token, created = Token.objects.get_or_create(user=user)
         return Response({
             "user": UserSerializer(user, context=self.get_serializer_context()).data,
-            # "token": Token.key,
+            "token": Token.key,
             "message": "User created successfully",
         })
 
